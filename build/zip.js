ed9ebdbe9cb4304beb49fb1ac3eb7f626067dece
adbenitez <asieldbenitez@gmail.com>
2022-04-24 01:48:10 -0400

    remove unused things

--- build/zip.js
+++ build/zip.js
@@ -9,7 +9,7 @@ const zip = () => ({
   name: 'zip',
   generateBundle(_, bundle) {
     const distPath = path.join(__dirname, '..', 'dist');
-    const zipPath = path.join(distPath, 'index.zip');
+    const zipPath = path.join(distPath, 'stolen-sword.xdc');
     const { source } = bundle['index.html'];
 
     if (!fs.existsSync(distPath)) fs.mkdirSync(distPath);
@@ -27,10 +27,6 @@ const zip = () => ({
       .on('finish', () => {
         execFile(advzip, ['--recompress', '--shrink-extra', zipPath], err => {
           console.log(err ? err : 'ZIP file minified!', filesize(fs.statSync(zipPath).size));
-          execFile(path.join(__dirname, 'ect'), ['-8', '-zip', zipPath], (err, stdout, stderr) => {
-            console.log(err ? stderr : stdout);
-            console.log(`File size: ${filesize(fs.statSync(zipPath).size)}\n`);
-          });
         });
       });
   }
